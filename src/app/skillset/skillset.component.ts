6e286782b41dff404bbdde20373ecd98d1ee08d1
James Li <james.li@opower.com>
2017-10-19 20:12:18 -0700

    Modifying bar chart

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -13,25 +13,40 @@ export class SkillsetComponent implements OnInit {
   ngOnInit() {
   }
 
+// Everything pie chart
+  public pieChartType:string = 'pie';
   public frontEndLabels:string[] = ['Angular 2/4', 'HTML', 'CSS', 'TypeScript', 'UI/UX'];
   public frontEndData:number[] = [200, 75, 65, 85, 70];
-  
   public frontEndLineLabels:Array<any> = ['Proficiency', 'Preference'];
   public frontEndlineData:Array<any> = [
     [90, 70, 55, 80, 60],
     [100, 70, 50, 60, 70]
   ];
-
   public backEndLabels:string[] = ['Node.js', 'Express', 'NoSQL', 'RDBMS', 'Python'];
   public backEndData:number[] = [200, 100, 200, 100, 50];
-
-  // public generalLabels:string[] = ['Technical Communications', 'JIRA', 'SCRUM', 'Agile Methodologies', 'Client Support'];
-
  
-  public pieChartType:string = 'pie';
+
+// Setting up variables for the bar chart
+  public barChartType:string = 'bar';
+  public barData:number[] = [2, 2, 3, 2, 3];
+  public barLabel:string[] = ['HTML', 'CSS', 'Angular 2/4', 'UX Design', 'etc'];
+  public barChartColors:Array<any> = [{
+    backgroundColor: 'rgba(150, 150, 240, 0.3)'
+  }];
+  public barOptions:any = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        stacked: true,
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
 
   public randomizeType():void {
-    this.pieChartType = this.pieChartType === 'line' ? 'pie' : 'line';
+    // this.pieChartType = this.pieChartType === 'line' ? 'pie' : 'line';
     // this.data = this.data === this.frontEndlineData? this.frontEndData : this.frontEndlineData;
   }
 
@@ -42,6 +57,8 @@ export class SkillsetComponent implements OnInit {
   public chartHovered(e:any):void {
     console.log(e);
   }
+
+
 
 }
 
